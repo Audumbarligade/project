5a63af4fa60bbc390e8c99034d312a661709f8d5
Audumbar Ligade <audumbar.b.ligade@gmail.com>
2022-11-01 15:11:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import {Button} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+     <h1>E-comm project</h1>
+     <button> normoal button</button>
+     <Button>Bootsrap Button</Button>
+    </div>
+  );
+}
+
+export default App;
